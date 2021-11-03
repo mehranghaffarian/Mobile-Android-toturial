c190d03e9ec8190913e997c4e192520b9bc83f59
mehran <mehranghaffarian1381@gmail.com>
2021-11-03 21:23:18 +0330

    db class created

---
+++ app/src/main/java/com/example/sqlite_second_project/DB.java
@@ -0,0 +1,23 @@
+package com.example.sqlite_second_project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DB extends SQLiteOpenHelper {
+    public DB(@Nullable Context context) {
+        super(context, "users", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
