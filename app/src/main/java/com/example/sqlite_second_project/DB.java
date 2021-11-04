deae98e1f2cafa3f7c815ac7ff22d197515a8821
mehran <mehranghaffarian1381@gmail.com>
2021-11-04 14:35:02 +0330

    project finished. almost the same

--- app/src/main/java/com/example/sqlite_second_project/DB.java
+++ app/src/main/java/com/example/sqlite_second_project/DB.java
@@ -1,23 +1,65 @@
 package com.example.sqlite_second_project;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class DB extends SQLiteOpenHelper {
+    public static final String CUSTOMERS_TABLE = "customersInfo";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String IS_ACTIVE = "isActive";
+    public static final String AGE = "age";
+
     public DB(@Nullable Context context) {
-        super(context, "users", null, 1);
+        super(context, CUSTOMERS_TABLE, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createCommand = "CREATE TABLE " + CUSTOMERS_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " TEXT, " + AGE + " INT, " + IS_ACTIVE + " BOOL)";
 
+        sqLiteDatabase.execSQL(createCommand);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public boolean insertCustomer(String name, int age, boolean is_active){
+        SQLiteDatabase sQlDb = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(NAME, name);
+        cv.put(AGE, age);
+        cv.put(IS_ACTIVE, is_active);
+
+        long res = sQlDb.insert(CUSTOMERS_TABLE, null, cv);
+
+        sQlDb.close();
+
+        return res != -1;
+    }
+
+    public StringBuffer getAll() {
+        SQLiteDatabase sql = this.getWritableDatabase();
+        Cursor res = sql.rawQuery("SELECT * FROM " + CUSTOMERS_TABLE, null);
+        StringBuffer buffer = new StringBuffer();
+
+        if(res.getCount() == 0)
+            buffer.append("There is no customer yet click add to add a customer.");
+
+        while (res.moveToNext()){
+            buffer.append("id: " + res.getString(0) + "\nname: " + res.getString(1) + "\nage: " + res.getString(2) + "\nis active: " + (res.getInt(3) == 1 ? "YES" : "NO") + "\n\n");
+        }
+        res.close();
+        sql.close();
+
+        return buffer;
     }
 }
--- app/src/main/java/com/example/sqlite_second_project/MainActivity.java
+++ app/src/main/java/com/example/sqlite_second_project/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.sqlite_second_project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText name, id;
+    private EditText name, age;
     private Switch is_active;
     private Button view_all, add;
+    private DB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +23,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeElements();
-
+        db = new DB(this);
         setListeners();
     }
 
     private void setListeners() {
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().length() > 0 && name.getText().toString().length() <= 20 && Integer.parseInt(age.getText().toString()) > 4 && Integer.parseInt(age.getText().toString()) <= 150)
+                    if(db.insertCustomer(name.getText().toString(), Integer.parseInt(age.getText().toString()), is_active.isChecked()))
+                        Toast.makeText(MainActivity.this, "Customer added successfully", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(MainActivity.this, "something went wrong please try again", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "name and age must be between 1, 20 characters and 5, 150", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        view_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Matches");
+                builder.setMessage(db.getAll());
+                builder.show();
+            }
+        });
     }
 
     private void initializeElements() {
         name = findViewById(R.id.customer_name);
-        id = findViewById(R.id.customer_id);
+        age = findViewById(R.id.customer_age);
         is_active = findViewById(R.id.is_active);
         view_all = findViewById(R.id.view_all);
         add = findViewById(R.id.add);
